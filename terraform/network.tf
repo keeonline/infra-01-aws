a3845fc534c898bd7bb6b7decdcf649ab4759ec7
keeonline <keeonline@outlook.com>
2025-03-16 16:30:48 +0000

    wip

--- terraform/network.tf
+++ terraform/network.tf
@@ -71,3 +71,15 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.main.id
 }
+
+resource "aws_route" "local" {
+  route_table_id            = aws_route_table.testing.id
+  destination_cidr_block    = aws_vpc.main.cidr_block
+  gateway_id = "local"
+}
+
+resource "aws_route" "igw" {
+  route_table_id            = aws_route_table.testing.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
