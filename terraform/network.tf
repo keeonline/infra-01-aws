4b158511f475aea1b287f120fae889a9da302e8d
keeonline <keeonline@outlook.com>
2025-03-16 14:52:52 +0000

    wip

--- terraform/network.tf
+++ terraform/network.tf
@@ -62,7 +62,7 @@ resource "aws_route_table" "public" {
 
 # Route table associations for both Public subnet
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public.id)
+  count          = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
