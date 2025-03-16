3246973c29f0a9fa8b6de5c5125b429b74f41b96
keeonline <keeonline@outlook.com>
2025-03-16 11:03:16 +0000

    wip

--- terraform/network.tf
+++ terraform/network.tf
@@ -5,3 +5,21 @@ resource "aws_vpc" "main" {
     Name = "${var.environment}-vpc"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "${var.environment}-subnet-public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "${var.environment}-subnet-private"
+  }
+}
