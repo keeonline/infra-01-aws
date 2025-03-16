80ba8946ab39cf2fce49806eee7b7434101b74ef
keeonline <keeonline@outlook.com>
2025-03-16 14:51:50 +0000

    wip

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,8 +1,11 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags = {
     Name = "${var.environment}-vpc"
+    Environment = "${var.environment}"
   }
 }
 
@@ -11,6 +14,7 @@ resource "aws_internet_gateway" "igw" {
 
   tags = {
     Name = "${var.environment}-igw"
+    Environment = "${var.environment}"
   }
 }
 
@@ -28,6 +32,7 @@ resource "aws_subnet" "public" {
 
   tags = {
     Name = "${var.environment}-subnet-public-${count.index}"
+    Environment = "${var.environment}"
   }
 }
 
@@ -39,5 +44,25 @@ resource "aws_subnet" "private" {
 
   tags = {
     Name = "${var.environment}-subnet-private-${count.index}"
+    Environment = "${var.environment}"
   }
+}
+
+
+
+# Routing tables to route traffic for Public Subnet
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.environment}-route-table-public"
+    Environment = "${var.environment}"
+  }
+}
+
+# Route table associations for both Public subnet
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public.id)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
