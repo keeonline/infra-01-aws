fb81fe7a2aaa5a6178876078505f5efd91397f27
keeonline <keeonline@outlook.com>
2025-03-17 13:18:22 +0000

    wip

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,4 +1,4 @@
-resource "aws_ecs_cluster" "foo" {
+resource "aws_ecs_cluster" "services" {
   name = "${var.environment}-ecs-cluster"
 
   tags = {
@@ -6,4 +6,23 @@ resource "aws_ecs_cluster" "foo" {
     Environment = "${var.environment}"
   }
 
+}
+
+resource "aws_ecs_task_definition" "chameleon" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "chameleon"
+      image     = "keeonline/chameleon:latest"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 18080
+        }
+      ]
+    }
+  ])
 }
