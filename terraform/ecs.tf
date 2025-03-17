a8baa69f50233bd48efa3aef749fa9a203a568ef
keeonline <keeonline@outlook.com>
2025-03-17 14:44:11 +0000

    wip

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,21 +1,24 @@
-resource "aws_ecs_cluster" "services" {
-  name = "${var.environment}-ecs-cluster"
+resource "aws_ecs_cluster" "chameleon" {
+  name = "${var.environment}-ecs-cluster-chameleon"
 
   tags = {
-    Name = "${var.environment}-ecs-cluster"
+    Name = "${var.environment}-ecs-cluster-chameleon"
     Environment = "${var.environment}"
   }
 
 }
 
-resource "aws_ecs_task_definition" "chameleon" {
+resource "aws_ecs_task_definition" "alpha" {
   family = "service"
   container_definitions = jsonencode([
     {
-      name      = "${var.environment}-chameleon-taskdef"
+      name      = "${var.environment}-taskdef-alpha"
       image     = "keeonline/chameleon:latest"
       cpu       = 10
       memory    = 512
+      "environment": [
+        {"name": "SERVICE_NAME", "value": "alpha"}
+      ],
       essential = true
       portMappings = [
         {
@@ -27,10 +30,42 @@ resource "aws_ecs_task_definition" "chameleon" {
   ])
 }
 
-resource "aws_ecs_service" "chameleon" {
-  name            = "${var.environment}-chameleon-service"
-  cluster         = aws_ecs_cluster.services.id
-  task_definition = aws_ecs_task_definition.chameleon.arn
+resource "aws_ecs_service" "alpha" {
+  name            = "${var.environment}-ecs-service-alpha"
+  cluster         = aws_ecs_cluster.chameleon.id
+  task_definition = aws_ecs_task_definition.alpha.arn
+  desired_count   = 1
+#  iam_role        = aws_iam_role.foo.arn
+#  depends_on      = [aws_iam_role_policy.foo]
+
+}
+
+resource "aws_ecs_task_definition" "bravo" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "${var.environment}-taskdef-bravo"
+      image     = "keeonline/chameleon:latest"
+      cpu       = 10
+      memory    = 512
+      "environment": [
+        {"name": "SERVICE_NAME", "value": "bravo"}
+      ],
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 18080
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "bravo" {
+  name            = "${var.environment}-ecs-service-bravo"
+  cluster         = aws_ecs_cluster.chameleon.id
+  task_definition = aws_ecs_task_definition.bravo.arn
   desired_count   = 1
 #  iam_role        = aws_iam_role.foo.arn
 #  depends_on      = [aws_iam_role_policy.foo]
