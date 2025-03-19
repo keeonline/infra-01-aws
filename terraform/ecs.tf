259ff75f4d08eddd3671a4dddfb5068a2c240974
keeonline <keeonline@outlook.com>
2025-03-19 10:50:45 +0000

    wip

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -103,7 +103,7 @@ resource "aws_ecs_service" "bravo" {
   cluster         = aws_ecs_cluster.chameleon.id
   task_definition = aws_ecs_task_definition.bravo.arn
   desired_count   = 1
-  launch_type = FARGATE
+  launch_type = "FARGATE"
 #  iam_role        = aws_iam_role.foo.arn
 #  depends_on      = [aws_iam_role_policy.foo]
 
