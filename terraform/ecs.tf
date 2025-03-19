19194656ebb48ad6aac7592f5d4b458664107a9a
keeonline <keeonline@outlook.com>
2025-03-19 08:12:40 +0000

    wip

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -8,72 +8,77 @@ resource "aws_ecs_cluster" "chameleon" {
 
 }
 
-resource "aws_ecs_task_definition" "alpha" {
-  family = "chameleon"
-  network_mode = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu       = 256
-  memory    = 512
+resource "aws_alb_target_group" "bravo" {
+  name                 = "${var.environment}-tg-bravo"
+  port                 = 8080
+  protocol             = "HTTP"
+  vpc_id               = aws_vpc.main.id
+  deregistration_delay = 5
+  target_type          = "ip"
 
-  container_definitions = jsonencode([
-    {
-      name      = "${var.environment}-taskdef-alpha"
-      image     = "keeonline/chameleon:latest"
-      environment = [
-        {"name": "SERVICE_NAME", "value": "alpha"}
-      ],
-      essential = true
-      portMappings = [
-        {
-          containerPort = 8080
-          hostPort      = 8080
-        }
-      ]
-    }
-  ])
-
-  runtime_platform {
-    operating_system_family = "LINUX"
-    cpu_architecture        = "X86_64"
-  }
-}
-
-resource "aws_ecs_service" "alpha" {
-  name            = "${var.environment}-ecs-service-alpha"
-  cluster         = aws_ecs_cluster.chameleon.id
-  task_definition = aws_ecs_task_definition.alpha.arn
-  desired_count   = 1
-#  iam_role        = aws_iam_role.foo.arn
-#  depends_on      = [aws_iam_role_policy.foo]
-
+  # health_check {
+  #   healthy_threshold   = 2
+  #   unhealthy_threshold = 2
+  #   interval            = 60
+  #   matcher             = var.healthcheck_matcher
+  #   path                = var.healthcheck_endpoint
+  #   port                = "traffic-port"
+  #   protocol            = "HTTP"
+  #   timeout             = 30
+  # }
 }
 
 resource "aws_ecs_task_definition" "bravo" {
-  family = "chameleon"
+  family = "${var.environment}-family-chameleon"
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu       = 256
   memory    = 512
   container_definitions = jsonencode([
     {
-      name      = "${var.environment}-taskdef-bravo"
+      name      = "${var.environment}-task-bravo"
       image     = "keeonline/chameleon:latest"
       environment = [
         {"name": "SERVICE_NAME", "value": "bravo"}
       ],
+      cpu = 256
+      memory = 512
       essential = true
       portMappings = [
         {
           containerPort = 8080
           hostPort      = 8080
+          protocol = "tcp"
         }
       ]
     }
   ])
 
-  runtime_platform {
-    operating_system_family = "LINUX"
-    cpu_architecture        = "X86_64"
+}
+
+resource "aws_security_group" "chameleon" {
+  name        = "${var.environment}-sg-http-chameleon"
+  description = "Security group for ECS task running on Fargate"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow ingress traffic from ALB on HTTP only"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_http.id]
+  }
+
+  egress {
+    description = "Allow all egress traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name     = "${var.environment}-sg-http-chameleon"
   }
 }
 
@@ -85,4 +90,15 @@ resource "aws_ecs_service" "bravo" {
 #  iam_role        = aws_iam_role.foo.arn
 #  depends_on      = [aws_iam_role_policy.foo]
 
+  load_balancer {
+    target_group_arn = aws_alb_target_group.bravo.arn
+    container_name   = "${var.environment}-task-bravo"
+    container_port   = 8080
+  }
+
+    network_configuration {
+      security_groups  = [aws_security_group.chameleon.id]
+      subnets          = aws_subnet.private.*.id
+      assign_public_ip = false
+    }
 }
