be3ed41693bcff79ac64d12d1d997619f2406b28
keeonline <keeonline@outlook.com>
2025-03-19 09:19:43 +0000

    wip

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -11,7 +11,7 @@ data "aws_iam_policy_document" "assume_role" {
 
 resource "aws_iam_role" "ecs_task_exec" {
     name                  = "${var.environment}-iam-role-ecs-task-exec"
-    assume_role_policy    = data.aws_iam_policy_document.assume_role_policy.json
+    assume_role_policy    = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_exec" {
@@ -21,7 +21,7 @@ resource "aws_iam_role_policy_attachment" "ecs_task_exec" {
 
 resource "aws_iam_role" "ecs_task" {
     name                  = "${var.environment}-iam-role-ecs-task"
-    assume_role_policy    = data.aws_iam_policy_document.assume_role_policy.json   
+    assume_role_policy    = data.aws_iam_policy_document.assume_role.json   
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task" {
