a1beb42ec4d2189b9049023161ad30966ea0f7c8
keeonline <keeonline@outlook.com>
2025-03-20 15:17:06 +0000

    wip

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -8,8 +8,8 @@ resource "aws_ecs_cluster" "chameleon" {
 
 }
 
-resource "aws_lb_target_group" "bravo" {
-  name                 = "${var.environment}-tg-bravo"
+resource "aws_lb_target_group" "chameleon" {
+  name                 = "${var.environment}-tg-chameleon"
   port                 = 8080
   protocol             = "HTTP"
   vpc_id               = aws_vpc.main.id
@@ -21,28 +21,28 @@ resource "aws_lb_target_group" "bravo" {
     unhealthy_threshold = 3
     interval            = 10
     matcher             = "200-299"
-    path                = "/bravo/actuator/health"
+    path                = "/chameleon/actuator/health"
     port                = 8080
     protocol            = "HTTP"
     timeout             = 6
   }
 }
 
-resource "aws_lb_listener_rule" "bravo" {
+resource "aws_lb_listener_rule" "chameleon" {
   listener_arn = aws_lb_listener.http.arn
   priority     = 20
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.bravo.arn
+    target_group_arn = aws_lb_target_group.chameleon.arn
   }
   condition {
     path_pattern {
-      values = ["/bravo/*"]
+      values = ["/chameleon/*"]
      }
    }
 }
 
-resource "aws_ecs_task_definition" "bravo" {
+resource "aws_ecs_task_definition" "chameleon" {
   family = "${var.environment}-family-chameleon"
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
@@ -52,12 +52,12 @@ resource "aws_ecs_task_definition" "bravo" {
   task_role_arn            = aws_iam_role.ecs_task.arn
   container_definitions = jsonencode([
     {
-      name      = "${var.environment}-task-bravo"
+      name      = "${var.environment}-task-chameleon"
       image     = "docker.io/keeonline/chameleon:latest"
-      environment = [
-        {"name": "SERVICE_NAME", "value": "bravo"},
-        {"name": "NOT_USED", "value": "n/a"}
-      ],
+      # environment = [
+      #   {"name": "SERVICE_NAME", "value": "chameleon"},
+      #   {"name": "NOT_USED", "value": "n/a"}
+      # ]
       cpu = 256
       memory = 512
       essential = true
@@ -78,21 +78,21 @@ resource "aws_security_group" "chameleon" {
   description = "Security group for ECS task running on Fargate"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description     = "Allow application ingress traffic from ALB on HTTP only"
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
-  }
+  # ingress {
+  #   description     = "Allow application ingress traffic from ALB on HTTP only"
+  #   from_port       = 8080
+  #   to_port         = 8080
+  #   protocol        = "tcp"
+  #   security_groups = [aws_security_group.alb.id]
+  # }
 
-  ingress {
-    description     = "Allow actuator ingress traffic from ALB on HTTP only"
-    from_port       = 9080
-    to_port         = 9080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
-  }
+  # ingress {
+  #   description     = "Allow actuator ingress traffic from ALB on HTTP only"
+  #   from_port       = 9080
+  #   to_port         = 9080
+  #   protocol        = "tcp"
+  #   security_groups = [aws_security_group.alb.id]
+  # }
 
   egress {
     description = "Allow all egress traffic"
@@ -107,18 +107,32 @@ resource "aws_security_group" "chameleon" {
   }
 }
 
-resource "aws_ecs_service" "bravo" {
-  name            = "${var.environment}-ecs-service-bravo"
+resource "aws_vpc_security_group_ingress_rule" "chameleon" {
+  security_group_id = aws_security_group.chameleon.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource "aws_vpc_security_group_egress_rule" "chameleon" {
+  security_group_id = aws_security_group.chameleon.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_ecs_service" "chameleon" {
+  name            = "${var.environment}-ecs-service-chameleon"
   cluster         = aws_ecs_cluster.chameleon.id
-  task_definition = aws_ecs_task_definition.bravo.arn
+  task_definition = aws_ecs_task_definition.chameleon.arn
   desired_count   = 1
   launch_type = "FARGATE"
 #  iam_role        = aws_iam_role.foo.arn
 #  depends_on      = [aws_iam_role_policy.foo]
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.bravo.arn
-    container_name   = "${var.environment}-task-bravo"
+    target_group_arn = aws_lb_target_group.chameleon.arn
+    container_name   = "${var.environment}-task-chameleon"
     container_port   = 8080
   }
 
