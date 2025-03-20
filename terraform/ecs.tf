17839e66c0298ddeebf5cb11380357206cc70332
keeonline <keeonline@outlook.com>
2025-03-20 15:23:20 +0000

    wip

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -94,13 +94,13 @@ resource "aws_security_group" "chameleon" {
   #   security_groups = [aws_security_group.alb.id]
   # }
 
-  egress {
-    description = "Allow all egress traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # egress {
+  #   description = "Allow all egress traffic"
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = -1
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   tags = {
     Name     = "${var.environment}-sg-http-chameleon"
