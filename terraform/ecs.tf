4b0afe42cc01c315840d4f2b03b49c4dc9be7d7b
keeonline <keeonline@outlook.com>
2025-03-20 16:24:54 +0000

    wip

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -46,8 +46,8 @@ resource "aws_ecs_task_definition" "chameleon" {
   family = "${var.environment}-family-chameleon"
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu       = 256
-  memory    = 512
+  cpu       = 1024
+  memory    = 2048
   execution_role_arn       = aws_iam_role.ecs_task_exec.arn
   task_role_arn            = aws_iam_role.ecs_task.arn
   container_definitions = jsonencode([
