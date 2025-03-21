58d27d919f0b5b008fa265c87a375fed6e2992fa
keeonline <keeonline@outlook.com>
2025-03-21 06:50:53 +0000

    wip

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -28,6 +28,18 @@ resource "aws_lb_target_group" "chameleon" {
   }
 }
 
+#############  REALLY IMPORTANT !!!  Add an OUTBOUND SG rule to the ALB SECURITY GROUP so the ALB can access the service
+
+resource "aws_vpc_security_group_egress_rule" "alb_chameleon" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 8080
+  to_port           = 8080
+  ip_protocol       = "tcp"
+}
+
+
+
 resource "aws_lb_listener_rule" "chameleon" {
   listener_arn = aws_lb_listener.http.arn
   priority     = 20
