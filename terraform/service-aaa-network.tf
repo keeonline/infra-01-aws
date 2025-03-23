8ea4b2ac99a28e9dac4c1ff7e686c23d132d7188
keeonline <keeonline@outlook.com>
2025-03-23 11:53:28 +0000

    wip

--- terraform/service-aaa-network.tf
+++ terraform/service-aaa-network.tf
@@ -2,7 +2,6 @@ data "aws_lb" "alb" {
   name = "${var.environment}-alb"
 }
 
-
 data "aws_subnets" "public" {
   filter {
     name   = "tag:Environment"
@@ -10,8 +9,6 @@ data "aws_subnets" "public" {
   }
 
   filter {
-    # name   = "tag:Name"
-    # values = ["${var.environment}-subnet-public-*"]
     name   = "tag:Public"
     values = ["yes"]
   }
@@ -24,9 +21,31 @@ data "aws_subnet" "public" {
   id       = each.value
 }
 
+data "aws_subnets" "private" {
+  filter {
+    name   = "tag:Environment"
+    values = ["${var.environment}"]
+  }
+
+  filter {
+    name   = "tag:Public"
+    values = ["no"]
+  }
+
+  depends_on = [data.aws_lb.alb]
+}
+
+data "aws_subnet" "private" {
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.value
+}
+
 locals {
   public_subnet_cidr_blocks = [for s in data.aws_subnet.public : s.cidr_block]
+  private_subnet_cidr_blocks = [for s in data.aws_subnet.private : s.cidr_block]
 }
+
+# Create a security group for use by services that allow traffic ingress and access to the NAT gateway
 
 resource "aws_security_group" "service" {
   name        = "${var.environment}-sg-service"
@@ -76,3 +95,36 @@ resource "aws_vpc_security_group_egress_rule" "service" {
     Environment = "${var.environment}"
   }
 }
+
+
+
+# Add egress rules to ALB so that it can talk to services in the private subnet
+
+resource "aws_vpc_security_group_egress_rule" "alb_egress_services" {
+  count             = length(local.private_subnet_cidr_blocks)
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = local.private_subnet_cidr_blocks[count.index]
+  from_port         = 8080
+  to_port           = 8080
+  ip_protocol       = "tcp"
+
+  tags = {
+    Name        = "${var.environment}-sg-alb-egress-services-${count.index}"
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_egress_management" {
+  count             = length(local.private_subnet_cidr_blocks)
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = local.private_subnet_cidr_blocks[count.index]
+  from_port         = 9080
+  to_port           = 9080
+  ip_protocol       = "tcp"
+
+  tags = {
+    Name        = "${var.environment}-sg-alb-egress-management-${count.index}"
+    Environment = "${var.environment}"
+  }
+}
+
