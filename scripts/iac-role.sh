d6f096cd744d3a56a77368ac3f4bbaa5427a6325
keeonline <48411999+keeonline@users.noreply.github.com>
2025-04-03 15:12:07 +0100

    Update iac-role.sh

--- scripts/iac-role.sh
+++ scripts/iac-role.sh
@@ -2,6 +2,6 @@ aws iam create-open-id-connect-provider --url https://token.actions.githubuserco
 
 GITHUB_USER=keeonline
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-aws iam create-role --role-name IaC --assume-role-policy-document "{\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"Federated\":\"arn:aws:iam::${AWS_ACCOUNT_ID}:oidc-provider/token.actions.githubusercontent.com\"},\"Action\":\"sts:AssumeRoleWithWebIdentity\",\"Condition\":{\"StringEquals\":{\"token.actions.githubusercontent.com:aud\":\"sts.amazonaws.com\"},\"StringLike\":{\"token.actions.githubusercontent.com:sub\":[\"repo:${GITHUB_USER}/infra-01-aws:*\"]}}}]}"
+aws iam create-role --role-name IaC --assume-role-policy-document "{\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"Federated\":\"arn:aws:iam::${AWS_ACCOUNT_ID}:oidc-provider/token.actions.githubusercontent.com\"},\"Action\":\"sts:AssumeRoleWithWebIdentity\",\"Condition\":{\"StringEquals\":{\"token.actions.githubusercontent.com:aud\":\"sts.amazonaws.com\"},\"StringLike\":{\"token.actions.githubusercontent.com:sub\":[\"repo:${GITHUB_USER}/*:*\"]}}}]}"
 
 aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --role-name IaC
