859e5a56a6b8baa30a5123986629de8078d80d97
keeonline <keeonline@outlook.com>
2025-05-11 12:09:59 +0100

    wip

---
+++ terraform/igw.tf
@@ -0,0 +1,24 @@
+# Create an internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name    = "${var.infra_environment}-igw"
+    # Created = "${timestamp()}"
+  }
+}
+
+# Create a route table and add a route for public internet access via the gateway
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name    = "${var.infra_environment}-rt-public"
+  }
+}
+
---
+++ terraform/subnets.tf
@@ -0,0 +1,38 @@
+# Create the public subnets in the main VPC
+resource "aws_subnet" "public" {
+  # count             = length(data.aws_availability_zones.available.names)
+  count             = var.az_use_count
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, (0 + (16 * count.index)))
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name       = "${var.infra_environment}-subnet-public-${count.index}"
+    Visibility = "public"
+  }
+}
+
+# Associate each of the public subnets to the public route table to allow internet traffic
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# Create the private subnets in the main VPC
+resource "aws_subnet" "private" {
+  # count             = length(data.aws_availability_zones.available.names)
+  count             = var.az_use_count
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, (48 + (16 * count.index)))
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name       = "${var.infra_environment}-subnet-private-${count.index}"
+    Visibility = "private"
+  }
+}
+
+
+
