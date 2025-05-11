28a3e29adbaab387cfd3b56616238e5719f1cbd8
keeonline <48411999+keeonline@users.noreply.github.com>
2025-05-11 13:01:34 +0100

    Refactor (#16)
    * wip
    
    * wip

---
+++ terraform/ngw.tf
@@ -0,0 +1,41 @@
+# Create a NAT gateway in each public subnet so that internet access from private subnets can be configured (later)
+resource "aws_eip" "ngw" {
+  count  = length(aws_subnet.public)
+  domain = "vpc"
+
+  tags = {
+    Name    = "${var.infra_environment}-eip-ngw-${count.index}"
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count         = length(aws_subnet.public)
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name    = "${var.infra_environment}-ngw-${count.index}"
+  }
+}
+
+# Create a route table for the each of the NAT gateways and add a route for internet traffic
+resource "aws_route_table" "private" {
+  count  = length(aws_subnet.private)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw[count.index].id
+  }
+
+  tags = {
+    Name    = "${var.infra_environment}-rt-private-${count.index}"
+  }
+}
+
+# Associate the private subnets with the route table for the NAT gateway in the same AZ
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,3 +10,8 @@ variable "resource_category" {
   type = string
   default = "infrastructure"
 }
+
+variable "az_use_count" {
+  type = number
+  default = 2
+}
